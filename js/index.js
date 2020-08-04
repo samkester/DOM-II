52a883c92e5793c6aa53b1da400c14098f069fcd
Sam Kester <samkester@gmail.com>
2020-08-04 13:38:21 -0600

    Clicking on the top banner image toggles its height

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+document.querySelector("#banner-img").addEventListener("click", event => event.target.classList.toggle("mini"));
+
+//document.querySelector("#banner-img").addEventListener("click", event => event.target.setAttribute("src", nextImageFrom(event.target.getAttribute("src"))));
+document.querySelector("#img-1").addEventListener("click", event => event.target.style.border = "5px solid pink");
+
+
+function nextImageFrom(previous){
+    if(previous === "img/adventure.jpg") return "img/destination.jpg";
+    if(previous === "img/destination.jpg") return "img/fun.jpg";
+    if(previous === "img/fun.jpg") return "img/fun-bus.jpg";
+    if(previous === "img/fun-bus.jpg") return "img/adventure.jpg";
+}
