59b3a81f85ab3b1fa6188994092cb618d56d2c36
Sam Kester <samkester@gmail.com>
2020-08-04 14:28:17 -0600

    scroll main image #2 on mousewheel

--- js/index.js
+++ js/index.js
@@ -10,12 +10,23 @@ function nextImageFrom(previous){ // return the item from "images" after the ite
     return images[0]; // fallback if undefined
 }
 
+// event listener #1 - toggle the size of the top image on click
 document.querySelector("#banner-img").addEventListener("click", event => event.target.classList.toggle("mini"));
 
+// event listener #2 - cycle through images in the first main image on click
 document.querySelector("#img-1").addEventListener("click", event => {
     //console.log(event.target.src);
     event.target.src = nextImageFrom(event.target.src);
     //console.log(event.target.src);
+});
+
+// event listener #3 - scroll through the second main image on mousewheel
+let image2offset = 0;
+document.querySelector("#img-2").addEventListener("wheel", event => {
+    event.preventDefault(); // stops the page itself from scrolling
+    image2offset += event.deltaY;
+    console.log(image2offset);
+    event.target.style.objectPosition = `0 ${image2offset}px`;
 });
 
 //document.querySelector("#img-1").addEventListener("click", event => event.target.style.border = "5px solid pink");
