2d262f5efa6ee563ccc99ea5035bb0ad65c13131
Sam Kester <samkester@gmail.com>
2020-08-04 13:51:17 -0600

    Article image #1 cycles through images on click

--- js/index.js
+++ js/index.js
@@ -1,12 +1,23 @@
+let images = [];
+document.querySelectorAll("img").forEach(item => images.push(item.src)); // NPM does some... thing... when running that obfuscates all the image names, so fetch their new names
+//console.log(images);
+
+function nextImageFrom(previous){ // return the item from "images" after the item we pass in
+    const i = images.indexOf(previous);
+    //console.log(i);
+    if(i + 1 >= images.length) return images[0];
+    if(i >= 0) return images[i+1];
+    return images[0]; // fallback if undefined
+}
+
 document.querySelector("#banner-img").addEventListener("click", event => event.target.classList.toggle("mini"));
 
-//document.querySelector("#banner-img").addEventListener("click", event => event.target.setAttribute("src", nextImageFrom(event.target.getAttribute("src"))));
-document.querySelector("#img-1").addEventListener("click", event => event.target.style.border = "5px solid pink");
+document.querySelector("#img-1").addEventListener("click", event => {
+    //console.log(event.target.src);
+    event.target.src = nextImageFrom(event.target.src);
+    //console.log(event.target.src);
+});
+
+//document.querySelector("#img-1").addEventListener("click", event => event.target.style.border = "5px solid pink");
 
 
-function nextImageFrom(previous){
-    if(previous === "img/adventure.jpg") return "img/destination.jpg";
-    if(previous === "img/destination.jpg") return "img/fun.jpg";
-    if(previous === "img/fun.jpg") return "img/fun-bus.jpg";
-    if(previous === "img/fun-bus.jpg") return "img/adventure.jpg";
-}
